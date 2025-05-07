a0211deda4c89971c0523b5bc8fc1146dd26f6c6
Brady Newhard <brady.russell@gmail.com>
2025-05-07 00:00:40 -0400

    Fix login: disable authentication classes to prevent CSRF 403

--- reports/views/token_views.py
+++ reports/views/token_views.py
@@ -1,4 +1,4 @@
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
@@ -12,6 +12,7 @@ logger = logging.getLogger(__name__)
 
 @csrf_exempt
 @api_view(['POST'])
+@authentication_classes([])
 @permission_classes([AllowAny])
 def login(request):
     try:
