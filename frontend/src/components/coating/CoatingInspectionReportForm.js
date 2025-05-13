e621df53ef3386cc3f4ad8c4d40ccedbacdd03c2
Brady Newhard <brady.russell@gmail.com>
2025-05-12 22:31:22 -0400

    incremental formatting change

--- frontend/src/components/coating/CoatingInspectionReportForm.js
+++ frontend/src/components/coating/CoatingInspectionReportForm.js
@@ -41,16 +41,25 @@ const CoatingInspectionReportForm = () => {
       <Paper sx={{ p: 3, mt: 2, maxWidth: 1200, margin: '0 auto' }}>
         {/* Section 1: Project/Report Info */}
         <Typography variant="h6" sx={{ mb: 2 }}>Project & Report Information</Typography>
-        <Grid container spacing={2} sx={{ mb: 2 }}>
-          <Grid item xs={12} sm={6} md={4}><TextField label="Contractor" fullWidth /></Grid>
-          <Grid item xs={12} sm={6} md={4}><TextField label="Project No./WBS" fullWidth /></Grid>
-          <Grid item xs={12} sm={6} md={4}><TextField label="Facility ID" fullWidth /></Grid>
-          <Grid item xs={12} sm={6} md={4}><TextField label="OQ Personnel" fullWidth /></Grid>
-          <Grid item xs={12} sm={6} md={4}><TextField label="Report #" fullWidth /></Grid>
-          <Grid item xs={12} sm={6} md={4}><TextField label="Date" type="date" InputLabelProps={{ shrink: true }} fullWidth /></Grid>
-          <Grid item xs={12} sm={6} md={4}><TextField label="QC Inspector Start/Stop Time" fullWidth /></Grid>
-          <Grid item xs={12} sm={6} md={4}><TextField label="Crew Start/Stop Time" fullWidth /></Grid>
-          <Grid item xs={12} sm={6} md={4}><TextField label="General Location" fullWidth /></Grid>
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            width: '100%',
+            m: 0,
+            display: 'grid',
+            gridTemplateColumns: { xs: '1fr', sm: '1fr 1fr', md: '1fr 1fr 1fr' }
+          }}
+        >
+          <Grid item><TextField label="Contractor" fullWidth /></Grid>
+          <Grid item><TextField label="Project No./WBS" fullWidth /></Grid>
+          <Grid item><TextField label="Facility ID" fullWidth /></Grid>
+          <Grid item><TextField label="OQ Personnel" fullWidth /></Grid>
+          <Grid item><TextField label="Report #" fullWidth /></Grid>
+          <Grid item><TextField label="Date" type="date" InputLabelProps={{ shrink: true }} fullWidth /></Grid>
+          <Grid item><TextField label="QC Inspector Start/Stop Time" fullWidth /></Grid>
+          <Grid item><TextField label="Crew Start/Stop Time" fullWidth /></Grid>
+          <Grid item><TextField label="General Location" fullWidth /></Grid>
         </Grid>
         <Divider sx={{ my: 3 }} />
 
@@ -150,111 +159,192 @@ const CoatingInspectionReportForm = () => {
 
         {/* Section 1B: Surface Preparation */}
         <Typography variant="h6" sx={{ mb: 2 }}>Section 1B – Surface Preparation (Hold Point)</Typography>
-        <Grid container spacing={2} sx={{ mb: 2 }}>
-          <Grid item xs={12} sm={2}><TextField label="No." fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Location" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Surface Cleanliness" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Specified" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Actual" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Surface Profile" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Specified" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Actual" fullWidth /></Grid>
-          <Grid item xs={12} sm={4}><TextField label="Comments" fullWidth /></Grid>
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            width: '100%',
+            m: 0,
+            display: 'grid',
+            gridTemplateColumns: { xs: '1fr', sm: '1fr 1fr', md: 'repeat(6, 1fr)' }
+          }}
+        >
+          <Grid item><TextField label="No." fullWidth /></Grid>
+          <Grid item><TextField label="Location" fullWidth /></Grid>
+          <Grid item><TextField label="Surface Cleanliness" fullWidth /></Grid>
+          <Grid item><TextField label="Specified" fullWidth /></Grid>
+          <Grid item><TextField label="Actual" fullWidth /></Grid>
+          <Grid item><TextField label="Surface Profile" fullWidth /></Grid>
+          <Grid item><TextField label="Specified" fullWidth /></Grid>
+          <Grid item><TextField label="Actual" fullWidth /></Grid>
+          <Grid item><TextField label="Comments" fullWidth /></Grid>
         </Grid>
         <Divider sx={{ my: 3 }} />
 
         {/* Section 1C: Surface Preparation Checklist */}
         <Typography variant="h6" sx={{ mb: 2 }}>Section 1C – Surface Preparation Checklist</Typography>
-        <Grid container spacing={2} sx={{ mb: 2 }}>
-          <Grid item xs={12} sm={2}><TextField label="Checklist Item" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Yes" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="No" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="N/A" fullWidth /></Grid>
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            width: '100%',
+            m: 0,
+            display: 'grid',
+            gridTemplateColumns: { xs: '1fr', sm: '1fr 1fr', md: 'repeat(4, 1fr)' }
+          }}
+        >
+          <Grid item><TextField label="Checklist Item" fullWidth /></Grid>
+          <Grid item><TextField label="Yes" fullWidth /></Grid>
+          <Grid item><TextField label="No" fullWidth /></Grid>
+          <Grid item><TextField label="N/A" fullWidth /></Grid>
         </Grid>
         <Divider sx={{ my: 3 }} />
 
         {/* Section 2A: Coating Application */}
         <Typography variant="h6" sx={{ mb: 2 }}>Section 2A – Coating Application</Typography>
-        <Grid container spacing={2} sx={{ mb: 2 }}>
-          <Grid item xs={12} sm={1}><TextField label="No." fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Location" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Coating Type/Product ID" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Application Method (Spray/Brush)" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Application Time Begin" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Application Time End" fullWidth /></Grid>
-          <Grid item xs={12} sm={1}><TextField label="WFT Mils" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Comments" fullWidth /></Grid>
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            width: '100%',
+            m: 0,
+            display: 'grid',
+            gridTemplateColumns: { xs: '1fr', sm: '1fr 1fr', md: 'repeat(8, 1fr)' }
+          }}
+        >
+          <Grid item><TextField label="No." fullWidth /></Grid>
+          <Grid item><TextField label="Location" fullWidth /></Grid>
+          <Grid item><TextField label="Coating Type/Product ID" fullWidth /></Grid>
+          <Grid item><TextField label="Application Method (Spray/Brush)" fullWidth /></Grid>
+          <Grid item><TextField label="Application Time Begin" fullWidth /></Grid>
+          <Grid item><TextField label="Application Time End" fullWidth /></Grid>
+          <Grid item><TextField label="WFT Mils" fullWidth /></Grid>
+          <Grid item><TextField label="Comments" fullWidth /></Grid>
         </Grid>
         <Divider sx={{ my: 3 }} />
 
         {/* Section 2B: Mixing Report */}
         <Typography variant="h6" sx={{ mb: 2 }}>Section 2B – Mixing Report</Typography>
-        <Grid container spacing={2} sx={{ mb: 2 }}>
-          <Grid item xs={12} sm={1}><TextField label="Mix #" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Location" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Comp A Batch #" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Comp B Batch #" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Mix Time" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Act Induct Time" fullWidth /></Grid>
-          <Grid item xs={12} sm={1}><TextField label="Pot Life (min)" fullWidth /></Grid>
-          <Grid item xs={12} sm={1}><TextField label="Qty Used" fullWidth /></Grid>
-          <Grid item xs={12} sm={1}><TextField label="Witnessed By" fullWidth /></Grid>
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            width: '100%',
+            m: 0,
+            display: 'grid',
+            gridTemplateColumns: { xs: '1fr', sm: '1fr 1fr', md: 'repeat(9, 1fr)' }
+          }}
+        >
+          <Grid item><TextField label="Mix #" fullWidth /></Grid>
+          <Grid item><TextField label="Location" fullWidth /></Grid>
+          <Grid item><TextField label="Comp A Batch #" fullWidth /></Grid>
+          <Grid item><TextField label="Comp B Batch #" fullWidth /></Grid>
+          <Grid item><TextField label="Mix Time" fullWidth /></Grid>
+          <Grid item><TextField label="Act Induct Time" fullWidth /></Grid>
+          <Grid item><TextField label="Pot Life (min)" fullWidth /></Grid>
+          <Grid item><TextField label="Qty Used" fullWidth /></Grid>
+          <Grid item><TextField label="Witnessed By" fullWidth /></Grid>
         </Grid>
         <Divider sx={{ my: 3 }} />
 
         {/* Section 2C: Coating Application Checklist */}
         <Typography variant="h6" sx={{ mb: 2 }}>Section 2C – Coating Application Checklist</Typography>
-        <Grid container spacing={2} sx={{ mb: 2 }}>
-          <Grid item xs={12} sm={2}><TextField label="Checklist Item" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Yes" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="No" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="N/A" fullWidth /></Grid>
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            width: '100%',
+            m: 0,
+            display: 'grid',
+            gridTemplateColumns: { xs: '1fr', sm: '1fr 1fr', md: 'repeat(4, 1fr)' }
+          }}
+        >
+          <Grid item><TextField label="Checklist Item" fullWidth /></Grid>
+          <Grid item><TextField label="Yes" fullWidth /></Grid>
+          <Grid item><TextField label="No" fullWidth /></Grid>
+          <Grid item><TextField label="N/A" fullWidth /></Grid>
         </Grid>
         <Divider sx={{ my: 3 }} />
 
         {/* Section 3A: DFT Readings */}
         <Typography variant="h6" sx={{ mb: 2 }}>Section 3A – DFT Readings (Hold Point)</Typography>
-        <Grid container spacing={2} sx={{ mb: 2 }}>
-          <Grid item xs={12} sm={1}><TextField label="No." fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Location" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Specified" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Average" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Range" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Yes" fullWidth /></Grid>
-          <Grid item xs={12} sm={1}><TextField label="No" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Comments" fullWidth /></Grid>
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            width: '100%',
+            m: 0,
+            display: 'grid',
+            gridTemplateColumns: { xs: '1fr', sm: '1fr 1fr', md: 'repeat(8, 1fr)' }
+          }}
+        >
+          <Grid item><TextField label="No." fullWidth /></Grid>
+          <Grid item><TextField label="Location" fullWidth /></Grid>
+          <Grid item><TextField label="Specified" fullWidth /></Grid>
+          <Grid item><TextField label="Average" fullWidth /></Grid>
+          <Grid item><TextField label="Range" fullWidth /></Grid>
+          <Grid item><TextField label="Yes" fullWidth /></Grid>
+          <Grid item><TextField label="No" fullWidth /></Grid>
+          <Grid item><TextField label="Comments" fullWidth /></Grid>
         </Grid>
         <Divider sx={{ my: 3 }} />
 
         {/* Section 3B: Holiday Inspection (Hold Point) */}
         <Typography variant="h6" sx={{ mb: 2 }}>Section 3B – Holiday Inspection (Hold Point)</Typography>
-        <Grid container spacing={2} sx={{ mb: 2 }}>
-          <Grid item xs={12} sm={2}><TextField label="Inspection Location" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Jeep Calibration Verified?" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Number of Holidays" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Voltage" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Location of Repairs" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Comments" fullWidth /></Grid>
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            width: '100%',
+            m: 0,
+            display: 'grid',
+            gridTemplateColumns: { xs: '1fr', sm: '1fr 1fr', md: 'repeat(6, 1fr)' }
+          }}
+        >
+          <Grid item><TextField label="Inspection Location" fullWidth /></Grid>
+          <Grid item><TextField label="Jeep Calibration Verified?" fullWidth /></Grid>
+          <Grid item><TextField label="Number of Holidays" fullWidth /></Grid>
+          <Grid item><TextField label="Voltage" fullWidth /></Grid>
+          <Grid item><TextField label="Location of Repairs" fullWidth /></Grid>
+          <Grid item><TextField label="Comments" fullWidth /></Grid>
         </Grid>
         <Divider sx={{ my: 3 }} />
 
         {/* Section 3C: Instrument Record */}
         <Typography variant="h6" sx={{ mb: 2 }}>Section 3C – Instrument Record</Typography>
-        <Grid container spacing={2} sx={{ mb: 2 }}>
-          <Grid item xs={12} sm={2}><TextField label="Instrument" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Brand" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Model" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Serial Number" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Calibration Date" fullWidth /></Grid>
-          <Grid item xs={12} sm={2}><TextField label="Comments" fullWidth /></Grid>
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            width: '100%',
+            m: 0,
+            display: 'grid',
+            gridTemplateColumns: { xs: '1fr', sm: '1fr 1fr', md: 'repeat(6, 1fr)' }
+          }}
+        >
+          <Grid item><TextField label="Instrument" fullWidth /></Grid>
+          <Grid item><TextField label="Brand" fullWidth /></Grid>
+          <Grid item><TextField label="Model" fullWidth /></Grid>
+          <Grid item><TextField label="Serial Number" fullWidth /></Grid>
+          <Grid item><TextField label="Calibration Date" fullWidth /></Grid>
+          <Grid item><TextField label="Comments" fullWidth /></Grid>
         </Grid>
         <Divider sx={{ my: 3 }} />
 
         {/* Section 4: Comments/Signatures */}
         <Typography variant="h6" sx={{ mb: 2 }}>Section 4 – Comments / Signatures</Typography>
-        <Grid container spacing={2} sx={{ mb: 2 }}>
-          <Grid item xs={12} sm={6}><TextField label="Comments" fullWidth multiline minRows={2} /></Grid>
-          <Grid item xs={12} sm={6}><TextField label="Signature" fullWidth /></Grid>
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            width: '100%',
+            m: 0,
+            display: 'grid',
+            gridTemplateColumns: { xs: '1fr', sm: '1fr 1fr' }
+          }}
+        >
+          <Grid item><TextField label="Comments" fullWidth multiline minRows={2} /></Grid>
+          <Grid item><TextField label="Signature" fullWidth /></Grid>
         </Grid>
         <Box sx={{ display: 'flex', justifyContent: 'flex-end', mt: 3 }}>
           <Button variant="contained" color="primary">Submit</Button>
