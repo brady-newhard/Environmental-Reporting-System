a82ca6adcc1e0b7a9b89c3e083b3eed05364b456
Brady Newhard <brady.russell@gmail.com>
2025-05-16 04:26:13 -0400

    Deploy Issues

--- inspectionreports/wsgi.py
+++ inspectionreports/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'environmentalreport.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inspectionreports.settings')
 
 application = get_wsgi_application()
