9e8407d546e07a5ed6ed13c1ac2a6cbc3aadce5f
Brady Newhard <brady.russell@gmail.com>
2025-05-30 02:27:52 -0400

    Fix PunchlistsConfig AppConfig name for Django import (Heroku deploy fix)

--- disciplines/environmental/punchlists/apps.py
+++ disciplines/environmental/punchlists/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 class PunchlistsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'punchlists' 
+    name = 'disciplines.environmental.punchlists' 
