a551043e0bfa8899c6138ee61cda8e947c4531bc
Brady Newhard <brady.russell@gmail.com>
2025-06-03 21:05:26 -0400

    Fix static file serving in production

--- inspectionreports/urls.py
+++ inspectionreports/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from users.views_auth import CustomAuthToken
 from django.views.generic import TemplateView
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,6 +32,12 @@ urlpatterns = [
     path('api/utility/', include('disciplines.utility.urls')),
     path('api/login/', CustomAuthToken.as_view(), name='api_token_auth'),
 ]
+
+# Serve static files in production
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    ]
 
 urlpatterns += [
     re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
