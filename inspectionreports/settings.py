380f76c63e8d3fa1734f9a7b53d72f9bcab0cc39
Brady Newhard <brady.russell@gmail.com>
2025-06-06 12:08:47 -0400

    Add rest_framework_nested to INSTALLED_APPS

--- inspectionreports/settings.py
+++ inspectionreports/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'rest_framework_nested',
     'corsheaders',
     'core',  # Core functionality
     'users',  # User management
