a03a92879574639eac8b8bb68b36288f85c9a4de
Brady Newhard <brady.russell@gmail.com>
2025-06-07 01:43:06 -0400

    Update Vite config for proper asset injection

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,10 +17,16 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    manifest: true,
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'index.html'),
       },
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
     },
   },
   publicDir: 'public',
