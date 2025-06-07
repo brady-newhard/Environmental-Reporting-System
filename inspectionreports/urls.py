981c1788f72e85ed4b7bf399949d626c3dac67a7
Brady Newhard <brady.russell@gmail.com>
2025-06-07 10:54:32 -0400

    Update static file serving configuration

--- inspectionreports/urls.py
+++ inspectionreports/urls.py
@@ -21,7 +21,23 @@ from django.conf.urls.static import static
 from users.views_auth import CustomAuthToken
 from django.views.generic import TemplateView
 from django.views.static import serve
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 import os
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Environmental Reporting API",
+        default_version='v1',
+        description="API for Environmental Reporting System",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,23 +48,59 @@ urlpatterns = [
     path('api/welding/', include('disciplines.welding.urls')),
     path('api/utility/', include('disciplines.utility.urls')),
     path('api/login/', CustomAuthToken.as_view(), name='api_token_auth'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 # Serve static files in production
 if not settings.DEBUG:
     urlpatterns += [
         re_path(r'^staticfiles/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
-        re_path(r'^assets/(?P<path>.*)$', serve, {'document_root': os.path.join(settings.STATIC_ROOT, 'assets')}),
-        re_path(r'^favicon\.ico$', serve, {'path': 'favicon.ico', 'document_root': settings.STATIC_ROOT}),
-        re_path(r'^manifest\.json$', serve, {'path': 'manifest.json', 'document_root': settings.STATIC_ROOT}),
-        re_path(r'^logo192\.png$', serve, {'path': 'logo192.png', 'document_root': settings.STATIC_ROOT}),
-        re_path(r'^logo512\.png$', serve, {'path': 'logo512.png', 'document_root': settings.STATIC_ROOT}),
+        re_path(r'^staticfiles/assets/(?P<path>.*)$', serve, {'document_root': os.path.join(settings.STATIC_ROOT, 'assets')}),
+        re_path(r'^staticfiles/favicon.ico$', serve, {'path': 'favicon.ico', 'document_root': settings.STATIC_ROOT}),
+        re_path(r'^staticfiles/manifest.json$', serve, {'path': 'manifest.json', 'document_root': settings.STATIC_ROOT}),
+        re_path(r'^staticfiles/logo192.png$', serve, {'path': 'logo192.png', 'document_root': settings.STATIC_ROOT}),
+        re_path(r'^staticfiles/logo512.png$', serve, {'path': 'logo512.png', 'document_root': settings.STATIC_ROOT}),
+        re_path(r'^staticfiles/robots.txt$', serve, {'path': 'robots.txt', 'document_root': settings.STATIC_ROOT}),
+    ]
+    
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
+    
+    urlpatterns += [
+        re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
     ]
 else:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+    urlpatterns += [
+        re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+    ]
 
-# Catch-all route for React frontend (must be last)
-urlpatterns += [
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
-]
+# Debug route to check static files
+if settings.DEBUG:
+    from django.http import JsonResponse
+    from django.views.decorators.csrf import csrf_exempt
+    import os
+
+    @csrf_exempt
+    def check_static_files(request):
+        static_root = settings.STATIC_ROOT
+        assets_dir = os.path.join(static_root, 'assets')
+        
+        files = {
+            'static_root': static_root,
+            'assets_dir': assets_dir,
+            'static_root_exists': os.path.exists(static_root),
+            'assets_dir_exists': os.path.exists(assets_dir),
+            'static_root_contents': os.listdir(static_root) if os.path.exists(static_root) else [],
+            'assets_dir_contents': os.listdir(assets_dir) if os.path.exists(assets_dir) else [],
+        }
+        
+        return JsonResponse(files)
+
+    urlpatterns += [
+        path('debug/static-files/', check_static_files),
+    ]
