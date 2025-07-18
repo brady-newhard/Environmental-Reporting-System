e32c67f024bdfbbbcbd1a8726f2be25b609b09f4
Brady Newhard <brady.russell@gmail.com>
2025-07-18 00:26:25 -0400

    Fix API URL to explicitly point to Django server - Change API_URL from /api to http://localhost:8000/api - This fixes the proxy issue where API calls were going to port 3000 instead of 8000 - Should resolve the 401 Unauthorized errors

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_URL = import.meta.env.VITE_API_URL && import.meta.env.VITE_API_URL !== ''
   ? `${import.meta.env.VITE_API_URL}/api` 
-  : '/api';
+  : 'http://localhost:8000/api';
 
 console.log('API_URL:', API_URL);
 
